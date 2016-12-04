0f25dfb57e5c02157e617867d107afe16bb1a421
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-12-04 10:13:01 +0000

    package.json fix

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,82 @@
-console.error("SyntaxError: {\n  \"name\": \"file-metadata-express-service\",\n  \"version\": \"1.0.0\",\n  \"description\": \"simple microservice returning a files size\",\n  \"main\": \"server.js\",\n  \"scripts\": {\n    \"test\": \"echo \\\"Error: no test specified\\\" && exit 1\",\n    \"start\": \"node server.js\",\n    \"nstart\": \"nodemon server.js\",\n    \"pug\": \"pug ./app/index.pug -o ./dist/\",\n    \"watch-pug\": \"pug -w ./app/index.pug -o ./dist/\",\n    \"css\": \"stylus ./app/*.styl -o ./dist/\",\n    \"watch-css\": \"stylus -w ./app/*.styl -o ./dist/\",\n    \"browserify\": \"browserify ./app/index.js -o ./dist/bundle.js\",\n    \"watch-browserify\": \"watchify ./app/index.js -o ./dist/bundle.js\",\n    \"build\": \"npm-run-all --parallel pug css browserify\",\n    \"start-dev\": \"npm-run-all --parallel watch-pug watch-css watch-browserify nstart\"\n  },\n  \"author\": \"Jonathan T L Lee\",\n  \"repository\": \"https://github.com/Lee182/fcc-file-microservice.git\"\n  \"license\": \"MIT\",\n  \"dependencies\": {\n    \"express\": \"^4.14.0\",\n    \"multer\": \"^1.2.0\"\n  },\n  \"devDependencies\": {\n    \"browserify\": \"^13.1.1\",\n    \"npm-run-all\": \"^3.1.2\",\n    \"pug\": \"^2.0.0-beta6\",\n    \"stylus\": \"^0.54.5\",\n    \"watchify\": \"^3.7.0\"\n  }\n}\n : Unexpected string in JSON at position 882");
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// form#uploadfile
+//   input(type='file', name='uploadFile')
+
+// form = document.querySelector('#uploadfile')
+// form.addEventListener('change', function(e){
+//   e.preventDefault()
+//   var file = e.target.files[0]
+//   if (typeof file !== 'object') return false
+//   upload(file, function(res) {
+//     console.log(res)
+//   })
+// })
+
+function upload({file, cb, errcb, progresscb, cookies, url}) {
+  var formData = new FormData()
+  formData.append('file', file)
+  var req = new XMLHttpRequest()
+  req.onreadystatechange = function (e) {
+    if (req.readyState === 4) {
+      cb(req.response)
+    }
+  }
+  req.onerror = errcb
+  req.addEventListener('progress', function(e){
+    console.log('progress',  Math.ceil(e.loaded/e.total) * 100 + '%')
+    if (typeof progresscb === 'function') {
+      progresscb(e)
+    }
+  }, false)
+  req.withCredentials = Boolean(cookies)
+  req.open('POST', url)
+  // req.setRequestHeader('Content-Type', 'multipart/form-data')
+  req.send(formData)
+  return req
+}
+
+function fileSize(bytes) {
+  var i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B',
+  'kB',
+  'MB',
+  'GB',
+  'TB'][i]
+}
+
+module.exports = {
+  fileSize: fileSize,
+  upload: upload
+}
+
+},{}],2:[function(require,module,exports){
+window.fileupload = require('./fileupload.js')
+$form = document.querySelector('.fileupload-form')
+$input = document.querySelector('.fileupload-input')
+$fileinfo = document.querySelector('.fileinfo')
+
+$form.reset()
+
+$form.addEventListener('submit', function(e){
+  e.preventDefault()
+  console.log('here?')
+  var file = $input.files[0]
+  console.log(file)
+  if (typeof file !== 'object') return false
+
+  var req = fileupload.upload({
+    url: '/uploadfile',
+    file: file,
+    cb: function(res) {
+      res = JSON.parse(res)
+      window.res = res
+      $fileinfo.textContent = JSON.stringify(res, null, 2)
+    },
+    errcb: function(err){
+      console.log('errcb',err)
+    }
+  })
+
+})
+
+},{"./fileupload.js":1}]},{},[2]);
